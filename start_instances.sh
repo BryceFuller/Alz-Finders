c37aa96ad055b2fc2062d144308a98afc6aaa7a3
kperilla <kperilla@utexas.edu>
2018-12-04 14:41:23 -0600

    More specific

--- start_instances.sh
+++ start_instances.sh
@@ -1,10 +1,9 @@
-
-## declare an array variable of all dns
+## declare an array variable of all ips
 declare -a arr=("element1" "element2" "element3")
 
-## now loop through the above array
+## now loop through the ips running the script
 for i in "${arr[@]}"
 do
-   scp -i <keypair> myfile.txt ubuntu@ec2-$i.com/home/ubuntu/myfile.txt
-   ssh -i "ds-test-key.pem" ubuntu@ec2-$i.us-west-2.compute.amazonaws.com screen -d -m touch testfile
+   scp -i <keypair> myfile.txt ubuntu@ec2-$i.us-west-2.compute.amazonaws.com/home/ubuntu/myfile.txt
+   ssh -i "ds-test-key.pem" ubuntu@ec2-$i.us-west-2.compute.amazonaws.com screen -d -m python thing.py
 done
